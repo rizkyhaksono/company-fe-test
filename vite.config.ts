2079255893d17efa993b3e25cfb4d2916d8b539e
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-02-18 22:21:46 +0700

    feat(vite): add allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,14 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 export default defineConfig({
   plugins: [
     vue(),
-	vueDevTools(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  preview: {
+    allowedHosts: ['company.natee.me'],
   }
 })
